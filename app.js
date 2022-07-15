18efa698568432395740adba2ec78ec6bc78f681
Anthony Malary <amalary@bu.edu>
2022-07-15 12:52:40 -0700

    section2 done

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-;
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(12, 5));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5; 
-console.log(combineValues(8, 8));
-addAndHandle(12, 18, function (result) {
-    console.log(result);
-});
+generateError("An error occured", 500);
