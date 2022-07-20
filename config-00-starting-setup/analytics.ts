29392e002867927f71216416a5b6d9e48e8b4adb
Anthony Malary <amalary@bu.edu>
2022-07-20 12:13:00 -0700

    Configs

---
+++ config-00-starting-setup/analytics.ts
@@ -0,0 +1,7 @@
+const nameUser = "Anthony"; 
+
+const ages = 12; 
+
+
+
+console.log("Sending...")
---
+++ config-00-starting-setup/app.ts
@@ -0,0 +1,7 @@
+const button = document.querySelector("button")!;
+
+button?.addEventListener("click", () =>{
+    console.log("Hey you clicked me !!!!!")
+});
+
+
