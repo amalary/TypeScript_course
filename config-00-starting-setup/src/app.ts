9b2801d6ddb1e6fb67703b14a47a4a8e7b678532
Anthony Malary <amalary@bu.edu>
2022-07-24 16:37:49 -0700

    Getters and setters

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -39,8 +39,25 @@ class ITDepartment extends Department {
 };
 
 class AccountingDepartment extends Department {
+    private lastReport:string;
+
+    get mosterRecentReport(){
+        if(this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found.")
+    }
+
+    set mostRecentReport(value:string){
+        if(!value){
+            throw new Error('Please pass in a valid value')
+        }
+        this.addReport(value);
+
+    }
     constructor(id:string, private reports:string[]){
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
     addEmployee(employee: string) {
 
@@ -53,12 +70,15 @@ class AccountingDepartment extends Department {
     }
     addReport(text:string){
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports(){
         console.log(this.reports)
     }
-}
+};
+
+
 
 
 
@@ -67,7 +87,10 @@ const it = new ITDepartment("R2D2",['Anthony'])
 const account = new AccountingDepartment("1234", [])
 
 console.log(it)
+
+account.mostRecentReport = "Thing seemm to be going smoothly"
 account.addReport("There seems to have been a miscalculation");
+console.log(account.mosterRecentReport)
 
 account.addEmployee('Anthony');
 account.addEmployee('Brian');
