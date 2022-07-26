1f4ac4cdbd1917910b3d2d18820a959ca6021931
Anthony Malary <amalary@bu.edu>
2022-07-26 13:18:53 -0700

    More on Type Guards

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -1,6 +1,6 @@
 type Admin = {
     name:string;
-    privilages: string[];
+    privileges: string[];
 };
 
 type Employee = {
@@ -12,7 +12,7 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
     name:'Anthony',
-    privilages:["create-server"],
+    privileges:["create-server"],
     startDate: new Date()
 };
 
@@ -21,3 +21,58 @@ type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
+
+
+function add(a: Combinable, b: Combinable){
+    if (typeof a === 'string' || typeof b === 'string'){
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee){
+    console.log('Name: ' + emp.name);
+    if('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if('startDate' in emp) {
+        console.log('Start Datae: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation({name: "Jake", startDate: new Date()});
+
+class Car {
+    drive(){
+        console.log("Driving...")
+    }
+}
+
+class Truck {
+    drive(){
+        console.log("Driving a truck...")
+    }
+
+    loadCargo(amount: number) {
+        console.log("Loading cargo ..." + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle){
+    vehicle.drive();
+
+    if(vehicle instanceof Truck){
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+
+useVehicle(v2)
